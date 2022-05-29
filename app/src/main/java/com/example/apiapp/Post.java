7e2d2a988ad584a4c90ce356ed9eed85b7ba1c6d
aunuj <aunujain53@gmail.com>
2022-05-29 09:47:33 +0530

    Retrofit

--- app/src/main/java/com/example/apiapp/Post.java
+++ app/src/main/java/com/example/apiapp/Post.java
@@ -1,5 +1,27 @@
 package com.example.apiapp;
 
-public class Post {
+import com.google.gson.annotations.SerializedName;
 
+public class Post {
+    private int usedId, id;
+    private String title;
+
+    @SerializedName("body")
+    private String text;
+
+    public int getUsedId() {
+        return usedId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getText() {
+        return text;
+    }
 }
